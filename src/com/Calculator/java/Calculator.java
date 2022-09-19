019b838c5728f836f1f0f8c7da3c8042b3aab05e
kapilmahajan123 <montyempire123gmail.com>
2022-09-19 19:40:52 +0530

    Removed: Substraction logic

---
+++ src/com/Calculator/java/Calculator.java
@@ -0,0 +1,20 @@
+package com.Calculator.java;
+
+public class Calculator {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Calculator!!!");
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 30;
+		int sum = num1 + num2;
+
+		int mul = num1 * num2 * num3;
+		int div = num1 / num2;
+		System.out.println("Sum is: " + sum);
+
+		System.out.println("Mul is: " + mul);
+		System.out.println("Div is: " + div);
+
+	}
+
+}
